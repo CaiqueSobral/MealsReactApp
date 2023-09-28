d72f84780c79e85834fe041c6e20ea7753e48495
Kiki <caiquelsobral@gmail.com>
2023-09-28 00:02:56 -0300

    Refac: refactor data object

--- src/data/dummyData.ts
+++ src/data/dummyData.ts
@@ -2,16 +2,16 @@ import Category from '../models/category';
 import Meal from '../models/meal';
 
 export const CATEGORIES = [
-  new Category('c1', 'Italian', '#f5428d'),
-  new Category('c2', 'Quick & Easy', '#f54242'),
-  new Category('c3', 'Hamburgers', '#f5a442'),
-  new Category('c4', 'German', '#f5d142'),
-  new Category('c5', 'Light & Lovely', '#368dff'),
-  new Category('c6', 'Exotic', '#41d95d'),
+  new Category('c1', 'Italian', '#FFC7C7'),
+  new Category('c2', 'Quick & Easy', '#F38181'),
+  new Category('c3', 'Hamburgers', '#F08A5D'),
+  new Category('c4', 'German', '#FBD85D'),
+  new Category('c5', 'Light & Lovely', '#279EFF'),
+  new Category('c6', 'Exotic', '#79AC78'),
   new Category('c7', 'Breakfast', '#9eecff'),
   new Category('c8', 'Asian', '#b9ffb0'),
   new Category('c9', 'French', '#ffc7ff'),
-  new Category('c10', 'Summer', '#47fced'),
+  new Category('c10', 'Summer', '#5CD2E6'),
 ];
 
 export const MEALS = [
