cf122c9a4dcc04dfc865826820ef36babab89424
Kiki <caiquelsobral@gmail.com>
2023-09-30 17:55:51 -0300

    Feat: Componatizate meals card

--- src/components/meals/MealCard.tsx
+++ src/components/meals/MealCard.tsx
@@ -1,17 +1,17 @@
+import Meal from '../../models/meal';
 import React from 'react';
 import { Text, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { Dimensions } from 'react-native/Libraries/Utilities/Dimensions';
 
 type Props = {
-  mealName: string;
-  height: number;
+  meal: Meal;
 };
 
 export default function MealCard(props: Props) {
   return (
-    <View style={{ height: props.height }} className="bg-slate-500 rounded-3xl">
-      <Text>{props.mealName}</Text>
+    <View className="flex-1 bg-slate-500 rounded-3xl">
+      <Text>{props.meal.title}</Text>
     </View>
   );
 }
