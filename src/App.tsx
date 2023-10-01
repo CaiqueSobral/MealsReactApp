78bde299a266d72f6b827f3648492621e2e8fd88
Kiki <caiquelsobral@gmail.com>
2023-10-01 01:35:07 -0300

    Feat: Add fontLoader async

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
 import { registerRootComponent } from 'expo';
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import CategoriesPage from './pages/CategoriesPage';
 import { StatusBar } from 'expo-status-bar';
 import RootNavigator from './routes/Index';
 import { NavigationContainer } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { Text } from 'react-native';
+
+SplashScreen.preventAutoHideAsync();
 
 export function App() {
+  let [fontsLoaded] = useFonts({
+    'LilitaOne': require('../assets/Fonts/LilitaOne.ttf'),
+    'Rubik': require('../assets/Fonts/Rubik-Regular.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <>
       <StatusBar style="dark" />
-      <SafeAreaProvider className="flex-1">
+      <SafeAreaProvider className="flex-1" onLayout={onLayoutRootView}>
         <NavigationContainer>
           <RootNavigator />
         </NavigationContainer>
---
+++ src/data/constants.ts
@@ -0,0 +1,7 @@
+export const FONTLILITA = {
+  fontFamily: 'LilitaOne',
+};
+
+export const FONTRUBIK = {
+  fontFamily: 'Rubik',
+};
